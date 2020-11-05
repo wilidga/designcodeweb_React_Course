02dc5a518818bb2a5474861294d0dbe6642b483d
wilman <wilidgasoft@gmail.com>
2020-11-05 12:03:09 -0600

    Commit props, buttons, icons
    Commit props, buttons, icons

--- src/components/sections/HeroSections.js
+++ src/components/sections/HeroSections.js
@@ -1,5 +1,9 @@
 import React from "react"
 import styled from "styled-components"
+import PurchaseButton from "../buttons/PurchaseButton"
+
+import { themes } from "../styles/ColorStyles"
+import { H1, MediumText } from "../styles/TextStyles"
 
 function HeroSection() {
   return (
@@ -14,6 +18,7 @@ function HeroSection() {
           Don´t skip design. Learn design and code, by building real apps with
           React and Swift. Complete courses about the best tools.
         </Description>
+        <PurchaseButton title="Start learning" subtitle="120+ hours of video" />
       </ContentWrapper>
     </Wrapper>
   )
@@ -34,12 +39,7 @@ const TextWrapper = styled.div`
   display: grid;
   gap: 30px;
 `
-const Title = styled.h1`
-  font-weight: bold;
-  font-size: 60px;
-  color: white;
+const Title = styled(H1)`
+  color: ${themes.dark.text1};
 `
-const Description = styled.p`
-  font-size: 17px;
-  line-height: 130%;
-`
+const Description = styled(MediumText)``
